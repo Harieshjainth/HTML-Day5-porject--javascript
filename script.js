4e7eb25bbf192465ac0bd28d6d669e64217b784f
Harieshjainth <harieshjainth@gmail.com>
2024-07-09 16:05:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('off').addEventListener('click',function(){
+    document.getElementById("image").src="https://c4.wallpaperflare.com/wallpaper/321/360/982/2012-bumblebee-camaro-transformers-wallpaper-preview.jpg"
+});
+document.getElementById('Transformation').addEventListener('click',function(){
+    document.getElementById("image").src="https://w0.peakpx.com/wallpaper/209/739/HD-wallpaper-bumble-bee-camaro-henry-recarte-bumble-bee-transformers-chevy-yellow-camaro.jpg"
+});
+document.getElementById('on').addEventListener('click',function(){
+    document.getElementById("image").src="https://c4.wallpaperflare.com/wallpaper/465/595/683/4k-bumblebee-transformers-wallpaper-preview.jpg"
+});
+
